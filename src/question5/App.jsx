f74732005dd93a2d04e554d6e5634f82c3610eab
David Shore <david@foggymedia.com>
2024-02-29 12:59:08 +0100

    lösninar

---
+++ src/question5/App.jsx
@@ -0,0 +1,13 @@
+// 5. Navbar 3p
+// Skapa en navbar med titeln "Company name" längst till vänster och
+// Länkarna Home, Blog, About och Contact. längst till höger.
+// Vid mobil-vy ska länkarna gömmas och istället visas en hamburger-meny-knapp.
+// När man klickar på knappen ska menyn med länkarna visas i
+// en kolumn efter varandra vertikalt.
+// När man klickar igen ska länkarna gömmas.
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
